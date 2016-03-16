e94b2771bb2093848ba63656904131675eda6f12
Petter Holmström <petter@vaadin.com>
2016-03-16 17:19:25 +0200

    Add error handler

--- filter-based-security/src/main/java/org/vaadin/peholmst/samples/springsecurity/filterbased/SecuredUI.java
+++ filter-based-security/src/main/java/org/vaadin/peholmst/samples/springsecurity/filterbased/SecuredUI.java
@@ -1,9 +1,12 @@
 package org.vaadin.peholmst.samples.springsecurity.filterbased;
 
+import com.vaadin.server.DefaultErrorHandler;
+import com.vaadin.server.ErrorEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.vaadin.annotations.Push;
 import com.vaadin.annotations.Theme;
+import com.vaadin.server.ErrorHandler;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.shared.ui.ui.Transport;
 import com.vaadin.spring.annotation.SpringUI;
@@ -12,6 +15,7 @@ import com.vaadin.ui.Notification;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.ValoTheme;
+import org.springframework.security.access.AccessDeniedException;
 
 @SpringUI
 @Push(transport = Transport.LONG_POLLING) // Websocket would bypass the filter chain
@@ -38,5 +42,15 @@ public class SecuredUI extends UI {
         }));
         setContent(layout);
         getPage().setTitle("Vaadin and Spring Security Demo - Filter Based Security");
+        setErrorHandler(this::handleError);
+    }
+
+    private void handleError(com.vaadin.server.ErrorEvent event) {
+       Throwable t = DefaultErrorHandler.findRelevantThrowable(event.getThrowable());
+        if (t instanceof AccessDeniedException) {
+            Notification.show("You do not have permission to perform this operation", Notification.Type.WARNING_MESSAGE);
+        } else {
+            DefaultErrorHandler.doDefault(event);
+        }
     }
 }
